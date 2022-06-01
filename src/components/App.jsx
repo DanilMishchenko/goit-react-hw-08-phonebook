8979173323a49e6e20ac11c4721d122e4cbe8429
Danil Mishchenko <dan5679mi@gmail.com>
2022-06-02 00:02:15 +0300

    added fetching current user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from 'components/Layout/Layout';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { authOperations } from 'redux/auth';
+import { authOperations, authSelectors } from 'redux/auth';
 import NotFoundView from 'views/NotFoundView';
 import { lazy, Suspense } from 'react';
 import { Loader } from './Loader/Loader';
@@ -27,6 +27,7 @@ const PhonebookView = lazy(() =>
 
 export const App = () => {
   const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
 
   useEffect(() => {
     dispatch(authOperations.fetchCurrentUser());
@@ -34,38 +35,42 @@ export const App = () => {
 
   return (
     <>
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<HomeView />} />
-            <Route
-              path="register"
-              element={
-                <PublicRoute>
-                  <RegisterView />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="login"
-              element={
-                <PublicRoute>
-                  <LoginView />
-                </PublicRoute>
-              }
-            />
-            <Route
-              path="contacts"
-              element={
-                <PrivatRoute>
-                  <PhonebookView />
-                </PrivatRoute>
-              }
-            />
-            <Route path="*" element={<NotFoundView />} />
-          </Route>
-        </Routes>
-      </Suspense>
+      {isFetchingCurrentUser ? (
+        <Loader />
+      ) : (
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomeView />} />
+              <Route
+                path="register"
+                element={
+                  <PublicRoute>
+                    <RegisterView />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="login"
+                element={
+                  <PublicRoute>
+                    <LoginView />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="contacts"
+                element={
+                  <PrivatRoute>
+                    <PhonebookView />
+                  </PrivatRoute>
+                }
+              />
+              <Route path="*" element={<NotFoundView />} />
+            </Route>
+          </Routes>
+        </Suspense>
+      )}
     </>
   );
 };
